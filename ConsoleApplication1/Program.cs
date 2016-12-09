e7bdb93d7411757892baa5fefe3f9c1475d5373b
Paula Magalhães <pmagalhaesa@hotmail.com>
2016-12-08 23:16:04 -0200

    csharp_letra_tamanho_par_impar

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra;
+            int tamanho;
+
+            palavra = Console.ReadLine();
+
+            Console.WriteLine(palavra[0]);
+            Console.WriteLine(palavra[palavra.Length-1]);
+
+            tamanho = palavra.Length;
+
+            if (tamanho % 2 ==0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+
+            if ((palavra[0]=='a' ||palavra[0]=='e' || palavra[0]=='i'||palavra[0]=='o'|| palavra[0]=='u')
+                && (palavra[palavra.Length - 1] == 'a' || palavra[palavra.Length - 1] == 'e' || palavra[palavra.Length - 1] == 'i' || palavra[palavra.Length - 1] == 'o' || palavra[palavra.Length - 1] == 'u'))
+            {
+                Console.WriteLine("vogal");
+            }
+            else
+            {
+                Console.WriteLine("nao sao vogais");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
